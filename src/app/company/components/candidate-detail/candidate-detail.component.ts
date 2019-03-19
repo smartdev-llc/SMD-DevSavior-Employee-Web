55a5b1559b2cef0e503cfb76d699405fa5376ac3
larry <larry@enclave.vn>
2019-03-19 14:34:23 +0700

    [Company] Download CV Student

--- src/app/company/components/candidate-detail/candidate-detail.component.ts
+++ src/app/company/components/candidate-detail/candidate-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {CandidateService} from '../../../core/services/candidate/candidate.service';
 import {Candidate} from '../../../core/models/candidate';
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+declare  var $: any;
 
 @Component({
   selector: 'app-candidate-detail',
@@ -15,6 +18,7 @@ export class CandidateDetailComponent implements OnInit {
 
   isLoading = true;
   candidate: Candidate;
+  isLoadingBtnDownloadPDF = false;
   constructor(private activatedRoute: ActivatedRoute,
               private candidateService: CandidateService) { }
 
@@ -25,8 +29,29 @@ export class CandidateDetailComponent implements OnInit {
       this.candidateService.getCandidate(jobId, candidateId).subscribe((response: Candidate) => {
         this.isLoading = false;
         this.candidate = response;
-          console.log('response', this.candidate);
       });
+  }
+  public captureScreen(btnDownload: HTMLButtonElement) {
+    this.isLoadingBtnDownloadPDF = true;
+    console.log(btnDownload);
+
+    const data = $('#content')[0];
+    html2canvas(data).then(canvas => {
+        // Few necessary setting options
+        const imgWidth = 208;
+        const pageHeight = 295;
+        const imgHeight = canvas.height * imgWidth / canvas.width;
+        const heightLeft = imgHeight;
+
+        const contentDataURL = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+        const position = 0;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+        this.isLoadingBtnDownloadPDF = false;
+        const candidateFileName = 'CV_' + this.candidate.lastName + '_' + this.candidate.firstName + '.pdf';
+        pdf.save(candidateFileName); // Generated PDF
+      });
+
   }
 
 }
